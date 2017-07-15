2e7d9d8f5293a3d566b0d497e03d72d247dcb5ff
Khomotjo MODIPA <kmodipa@e4r13p4.jnb.42.fr>
2017-07-15 16:32:50 +0200

    first website upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*var skillset = document.getElementsByClassName('skillset');
+alert(skillset);*/
+
+function main(){
+  $('.soft-body').hide();
+  $('.soft-body').fadeIn(2000);
+}
+
+$(document).ready(main);
