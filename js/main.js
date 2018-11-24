4b00bf5d3220310099d3b43b1e4217abb0c0cf7b
kmodipa <kmodipa@student.42.fr>
2018-11-24 23:15:22 +0200

    removed old site files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-/*var skillset = document.getElementsByClassName('skillset');
-alert(skillset);*/
-
-function main(){
-  $('.soft-body').hide();
-  $('.soft-body').fadeIn(2000);
-}
-
-$(document).ready(main);
